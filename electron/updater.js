ac64e877bd5f1668fd8c1c9c9ccc2ec482d65b4a
Ramiro Saenz <saenzramiro@gmail.com>
2018-09-27 18:12:52 -0300

    0.6.0

--- electron/updater.js
+++ electron/updater.js
@@ -1,11 +1,11 @@
 const {app, ipcMain} = require('electron');
 const { autoUpdater } = require("electron-updater");
-const version = app.getVersion();
+const path = require('path');
 
 const initialize = (window) => {
 	const webContents = window.webContents;
 	const send = webContents.send.bind(window.webContents);
-	autoUpdater.on('checking-for-update', (event) => send('autoUpdater:checking-for-update:'));
+	autoUpdater.on('checking-for-update', (event) => send('autoUpdater:checking-for-update'));
 	autoUpdater.on('update-downloaded', (event, ...args) => send('autoUpdater:update-downloaded', ...args));
 	ipcMain.on('autoUpdater:quit-and-install', (event) => autoUpdater.quitAndInstall());
 	ipcMain.on('autoUpdater:check-for-updates', (event) => autoUpdater.checkForUpdates());
