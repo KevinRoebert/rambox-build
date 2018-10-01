3e0cee4cdf5e9b95114166761bd148e0030a294e
Ramiro Saenz <saenzramiro@gmail.com>
2018-10-01 17:12:34 -0300

    Test

--- electron/updater.js
+++ electron/updater.js
@@ -2,6 +2,8 @@ const {app, ipcMain} = require('electron');
 const { autoUpdater } = require("electron-updater");
 const path = require('path');
 
+autoUpdater.logger = require("electron-log")
+autoUpdater.logger.transports.file.level = "info"
 const initialize = (window) => {
 	const webContents = window.webContents;
 	const send = webContents.send.bind(window.webContents);
