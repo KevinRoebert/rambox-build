ca02749915ddce65a9997eca19846b1005592e3d
Ramiro Saenz <saenzramiro@gmail.com>
2018-10-01 19:41:28 -0300

    asd

--- electron/updater.js
+++ electron/updater.js
@@ -3,16 +3,30 @@ const { autoUpdater } = require("electron-updater");
 const path = require('path');
 
 autoUpdater.logger = require("electron-log")
-autoUpdater.logger.transports.file.level = "info"
+autoUpdater.logger.transports.file.level = "debug"
 const initialize = (window) => {
 	const webContents = window.webContents;
 	const send = webContents.send.bind(window.webContents);
-	autoUpdater.on('checking-for-update', (event) => send('autoUpdater:checking-for-update'));
-	autoUpdater.on('update-available', (event, ...args) => send('autoUpdater:update-available', event, ...args));
-	autoUpdater.on('update-not-available', (event, ...args) => send('autoUpdater:update-not-available', event, ...args));
-	autoUpdater.on('update-downloaded', (event, ...args) => send('autoUpdater:update-downloaded', event, ...args));
-	ipcMain.on('autoUpdater:quit-and-install', (event) => autoUpdater.quitAndInstall());
-	ipcMain.on('autoUpdater:check-for-updates', (event) => autoUpdater.checkForUpdates());
+	// autoUpdater.on('checking-for-update', (event) => send('autoUpdater:checking-for-update'));
+	// autoUpdater.on('update-available', (event, ...args) => send('autoUpdater:update-available', event, ...args));
+	// autoUpdater.on('update-not-available', (event, ...args) => send('autoUpdater:update-not-available', event, ...args));
+	// autoUpdater.on('update-downloaded', (event, ...args) => send('autoUpdater:update-downloaded', event, ...args));
+	// ipcMain.on('autoUpdater:quit-and-install', (event) => autoUpdater.quitAndInstall());
+	// ipcMain.on('autoUpdater:check-for-updates', (event) => autoUpdater.checkForUpdates());
+
+
+
+	autoUpdater.on('checking-for-update', (...args) => autoUpdater.logger.info(...args));
+	autoUpdater.on('update-available', (...args) => autoUpdater.logger.info(...args));
+	autoUpdater.on('update-not-available', (...args) => autoUpdater.logger.info(...args));
+	autoUpdater.on('update-downloaded', (...args) => autoUpdater.logger.info(...args));
+	ipcMain.on('autoUpdater:check-for-updates', (...args) => {
+		autoUpdater.logger.info(...args);
+		autoUpdater.checkForUpdates();
+	});
+
+
+
 };
 
 module.exports = {initialize};
