8e1bc0424b516db680e735b020f65b58ccfdc7fd
Ramiro Saenz <saenzramiro@gmail.com>
2018-10-01 18:46:46 -0300

    test

--- electron/updater.js
+++ electron/updater.js
@@ -8,7 +8,9 @@ const initialize = (window) => {
 	const webContents = window.webContents;
 	const send = webContents.send.bind(window.webContents);
 	autoUpdater.on('checking-for-update', (event) => send('autoUpdater:checking-for-update'));
-	autoUpdater.on('update-downloaded', (event, ...args) => send('autoUpdater:update-downloaded', ...args));
+	autoUpdater.on('update-available', (event, ...args) => send('autoUpdater:update-available', event, ...args));
+	autoUpdater.on('update-not-available', (event, ...args) => send('autoUpdater:update-not-available', event, ...args));
+	autoUpdater.on('update-downloaded', (event, ...args) => send('autoUpdater:update-downloaded', event, ...args));
 	ipcMain.on('autoUpdater:quit-and-install', (event) => autoUpdater.quitAndInstall());
 	ipcMain.on('autoUpdater:check-for-updates', (event) => autoUpdater.checkForUpdates());
 };
